4019a14f9f7f4a879cc5e5e5650a87bcbd452d05
Ravindu Jayaweera <ravindujayaweera99@gmail.com>
2024-07-10 14:36:40 +0530

    Edit Post Page made responsive

--- resources/views/updatepost.blade.php
+++ resources/views/updatepost.blade.php
@@ -7,21 +7,21 @@
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 text-center mb-6">
             <div>
-                {{ __('Edit this Blog') }}
+                {{ __('Edit Blog') }}
             </div>
         </h2>
 
-        <div class="flex justify-center items-center">
+        <div>
             <form action="{{ route('post.update', $post->id) }}" method="post"
-                class="flex flex-col justify-center items-center md:block border border-black/10 px-6 py-12 shadow-md"
+                class="flex flex-col lg:justify-center lg:items-center md:block border border-black/10 px-6 py-12 shadow-md"
                 enctype="multipart/form-data">
                 @csrf
                 @method('PUT')
-                <div class="flex justify-between items-center gap-16 mb-6">
+                <div class="flex flex-col mb-6">
                     <label for="title">Title: </label>
                     <input type="text" name="title" value="{{ $post->title }}" class="border p-2">
                 </div>
-                <div class="mb-3">
+                <div class="flex flex-col mb-6">
                     <label for="inputImage" class="form-label"><strong>Image:</strong></label>
                     <input type="file" name="image" class="form-control @error('image') is-invalid @enderror"
                         id="inputImage">
@@ -29,13 +29,14 @@
                         <div class="form-text text-red-500">{{ $message }}</div>
                     @enderror
                 </div>
-                <div class="flex justify-between items-center mb-6">
+                <div class="flex flex-col mb-6">
                     <label for="title">Content: </label>
                     <td><textarea name="body" value="{!! $post->body !!}"></textarea></td>
                     <!-- <input type="text" name="body" value="{{ $post->body }}" class="border p-2"> -->
                 </div>
                 <div class="flex justify-center items-center">
-                    <button type="submit" class="mt-6 md:mt-0 md:ml-6 bg-blue-500 text-white px-2 py-1 ">Update</button>
+                    <button type="submit"
+                        class="mt-6 md:mt-0 md:ml-6 bg-blue-500 text-white px-6 py-2 rounded">Update</button>
                 </div>
             </form>
 
